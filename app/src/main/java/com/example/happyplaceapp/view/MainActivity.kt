10ac9a9286aa7fe68a118232c820af1a47a1116f
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-01-06 18:15:29 +0700

    start

---
+++ app/src/main/java/com/example/happyplaceapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.happyplaceapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.happyplaceapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
